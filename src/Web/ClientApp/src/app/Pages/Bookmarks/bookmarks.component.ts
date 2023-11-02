fc77177a636f00d34df24788db2ad72a0b5d020b
Eweaa <88718116+Eweaa@users.noreply.github.com>
2023-11-02 11:34:53 +0200

    Fixed removeBookmark Method
    Fixed removeBookmark Method.
    Bookmarks reRender after removing a bookmark

--- src/Web/ClientApp/src/app/Pages/Bookmarks/bookmarks.component.ts
+++ src/Web/ClientApp/src/app/Pages/Bookmarks/bookmarks.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ArticleDto, ArticlesClient, BookmarkDto, BookmarksClient } from '../../web-api-client';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { ArticlesClient, BookmarkDto, BookmarksClient } from '../../web-api-client';
 
 @Component({
   selector: 'app-bookmarks',
@@ -7,9 +7,11 @@ import { ArticleDto, ArticlesClient, BookmarkDto, BookmarksClient } from '../../
   styleUrls: ['./bookmarks.component.scss']
 })
 export class BookmarksComponent {
-  constructor(private service: ArticlesClient, private bookmarkService: BookmarksClient) { }
+  constructor(private service: ArticlesClient, private bookmarkService: BookmarksClient, private changeDetectorRef: ChangeDetectorRef) { }
 
   data: Array<BookmarkB> = [];
+  newList: Array<BookmarkB> = [];
+
   ngOnInit() {
     this.service.getBookmarkArticles(1).subscribe(res => {
       // this.data = [];
@@ -19,15 +21,21 @@ export class BookmarksComponent {
         obj.isBookmarked = true;
         this.data.push(obj)
       }
-
+      this.newList = [...this.data];
       console.log(this.data)
     })
   }
 
   unBookmark = (b: BookmarkB) => {
-    console.log('unbookmarked');
-    this.bookmarkService.removeBookmark(b.id);
+    this.bookmarkService.removeBookmark(b.id).subscribe(() => console.log('removed'));
     b.isBookmarked = false;
+    // console.log(this.data);
+
+    for (let index = 0; index < this.data.length; index++) {
+      if(this.newList[index].id === b.id){
+        this.newList.splice(index, 1)
+      }
+    }
   } 
 }
 
